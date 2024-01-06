3280e9c6179a852582f1dfd13ef0b1062a3069f5
annagrnva <m-ippolitova@mail.ru>
2024-01-07 01:38:42 +0300

    interactions

---
+++ javascript-basics/2/5.data-types/1/string.js
@@ -0,0 +1,7 @@
+let name1 = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name1}` ); // hello Ilya
---
+++ javascript-basics/2/6.interaction/1/interactions.js
@@ -0,0 +1,2 @@
+let name1 = prompt ('What is your name?', "Alex");
+alert (     `Your name is ${name1}    `);
