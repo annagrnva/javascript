58dac20dc7c1ea6c4366c5ff7390cea18833a6d8
annagrnva <m-ippolitova@mail.ru>
2024-01-08 01:16:40 +0300

    if else-?

--- javascript-basics/2/10.if-branches/4/from-if-to-question.js
+++ javascript-basics/2/10.if-branches/4/from-if-to-question.js
@@ -1,2 +1,5 @@
-let result = (a + b < 4) ? 'Мало' : 'Много';
-
+let a = prompt('Введите а', '');
+let b = prompt('Введите b', '');
+let result;
+(a+b < 4) ? result = 'Мало' : result = 'Много';
+alert(result);
--- javascript-basics/2/10.if-branches/5/ifelse-question.js
+++ javascript-basics/2/10.if-branches/5/ifelse-question.js
@@ -1,11 +1,7 @@
+let login = prompt ( 'введите логин', '' )
 let message;
 
-if (login == 'Сотрудник') {
-  message = 'Привет';
-} else if (login == 'Директор') {
-  message = 'Здравствуйте';
-} else if (login == '') {
-  message = 'Нет логина';
-} else {
-  message = '';
-}
+login == 'Сотрудник' ? message ='Привет' :
+login == 'Директор' ? message ='Здравствуйте' :
+login == '' ? message ='Нет логина':
+alert (message);
