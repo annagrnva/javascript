d150d6227c1f08969cf3c26f68257c4fb9035d86
annagrnva <m-ippolitova@mail.ru>
2024-01-15 13:38:31 +0300

    switch

---
+++ javascript-basics/2/14.switch/1/change-switch-on-if.js
@@ -0,0 +1,18 @@
+let browser = prompt('введите браузер', '');
+
+if (browser == 'Edge') {
+
+    alert("You've got the Edge!");
+
+} else if (browser == 'Chrome' 
+|| browser == 'Firefox'
+|| browser == 'Safari' 
+|| browser == 'Opera') {
+
+    alert('Okay we support these browsers too');
+
+} else {
+
+    alert('We hope that this page looks ok!'); 
+}
+
