bfb9454e831ceb495565d0991d178b7d4e813140
annagrnva <m-ippolitova@mail.ru>
2024-01-30 23:49:38 +0300

    this method

---
+++ javascript-basics/4/2/2/calculator.js
@@ -0,0 +1,17 @@
+let calculator = {
+    read() {
+        let a = prompt("введите значение а", );
+        let b = prompt("введите значение b", );
+    },
+    sum() { return 
+        a + b 
+    },
+
+    mul() { return 
+        a * b
+  },
+}
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
