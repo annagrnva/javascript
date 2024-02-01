607a38a2c0140422948df049634a5461a2652e6f
annagrnva <m-ippolitova@mail.ru>
2024-02-01 23:28:26 +0300

    doing extra tasks

---
+++ javascript-basics/4/2/3/ladder.js
@@ -0,0 +1,53 @@
+// function myFunc(n) {
+//     let result = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//         result += 1;
+//         console.log(result);
+//     }
+
+
+// };
+// myFunc(5);
+
+// function myFunc() {
+
+//     for (let i = 1; i <= 20; i++) {
+//         let result = ''
+
+//         if (i % 3 === 0 && i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         } else if (i % 3 === 0) {
+//             console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//             console.log("Buzz");
+//         } else {
+//             console.log(i)
+//         };
+
+//     }
+// }
+
+// myFunc();
+
+// Напишите функцию, которая выводит через console.log все цифры от 1 до
+// 100, с двумя исключениями. Для чисел, нацело делящихся на 3, она должна
+// выводить ‘Fizz’, а для чисел, делящихся на 5 (но не на 3) – ‘Buzz’.
+
+// Когда сумеете – исправьте её так, чтобы она выводила «FizzBuzz» для всех
+// чисел, которые делятся и на 3 и на 5.
+// без конструкции "else" только "if"
+
+function myFunc() {
+
+    for (let i = 1; i <= 10; i++) {
+let result = ""
+        if (result+= i % 3 === 0 && i % 5 === 0);
+        console.log("FizzBuzz");
+    }
+}
+
+myFunc();
+
+// result += "илья";
+// result += " и аня";
