bf1e4ab68c49165bce390fa624497f7a4d2e58ad
annagrnva <m-ippolitova@mail.ru>
2024-03-11 00:10:35 +0300

    doing extra loop tasks adding additional task

--- javascript-basics/extra-tasks-cycle-while-for.js/task-7.js
+++ javascript-basics/extra-tasks-cycle-while-for.js/task-7.js
@@ -8,16 +8,32 @@
 // Ввод: 9
 // Вывод: 111222333444555666777888999
 
+// let result = '';
+// for (let i = 1; i <= 5; i++) {
+
+//     for (let j = 1; j <= 3; j++) {
+//      result += i ;
+//     };
+
+// };
+// console.log(result)
+
+
+
+// Написать программу которая выведет такую строку:
+
+// Пример:
+// Ввод: 5
+// Вывод: 11 12 13 21 22 23 31 32 33 41 42 43 51 52 53
+
+
 let result = '';
 for (let i = 1; i <= 5; i++) {
 
     for (let j = 1; j <= 3; j++) {
-     result += i ;
+        result += ' ' + i + j;
     };
 
 };
-console.log(result)
-
-
-
-
+ 
+console.log(result);
