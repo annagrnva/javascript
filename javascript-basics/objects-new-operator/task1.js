311c829ba10a732c57ac2180f498dd8b288b4832
annagrnva <m-ippolitova@mail.ru>
2024-04-30 23:54:02 +0300

    ladder task in this method

---
+++ javascript-basics/objects-new-operator/task1.js
@@ -0,0 +1,29 @@
+// Возможно ли создать функции A и B, чтобы new A() == new B()?
+
+// function A() { ... }
+// function B() { ... }
+
+// let a = new A();
+// let b = new B();
+
+// alert( a == b ); // true
+// Если да – приведите пример вашего кода.
+
+
+let anna = {
+    name: 'Anna'
+}
+function A(name) {
+   return this.name = name;
+     
+};
+
+function B(name) {
+   return  this.name = name;
+    
+};
+
+let a = new A();
+let b = new B();
+
+console.log(a == b)
--- javascript-basics/this-method/task3.js
+++ javascript-basics/this-method/task3.js
@@ -2,25 +2,28 @@
 
 let ladder = {
   step: 0,
-  up() {
+  up: function () {
     this.step++;
+    return this
   },
-  down() {
+  down: function() {
     this.step--;
+    return this
   },
   showStep: function() { // показывает текущую ступеньку
     console.log( this.step );
+    return this
   }
 };
+ladder.up().up().down().showStep().down().showStep(); // показывает 1 затем 0
 // Теперь, если нам нужно выполнить несколько последовательных вызовов, мы можем сделать это так:
 
-ladder.up();
-ladder.up();
-ladder.down();
-ladder.showStep(); // 1
-ladder.down();
-ladder.showStep(); // 0
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
+// ladder.down();
+// ladder.showStep(); // 0
 // Измените код методов up, down и showStep таким образом, 
 // чтобы их вызов можно было сделать по цепочке, например так:
 
-// ladder.up().up().down().showStep().down().showStep(); // показывает 1 затем 0
