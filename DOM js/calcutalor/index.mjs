6443af5e37a05e4fb1e6df2decdebc08823386cd
annagrnva <m-ippolitova@mail.ru>
2024-08-13 00:08:05 +0300

    fixing task with filling the input-form in interface errors

--- DOM js/calcutalor/index.mjs
+++ DOM js/calcutalor/index.mjs
@@ -1,6 +1,6 @@
 const number1Input = document.querySelector("#number1-input");
 const number2Input = document.querySelector("#number2-input");
-const sumInput = document.querySelector("#sum-input");
+const resultInput = document.querySelector("#result-input");
 const calculatorForm = document.querySelector("#calculator-form");
 
 calculatorForm.addEventListener("submit", function (event) {
@@ -9,14 +9,25 @@ calculatorForm.addEventListener("submit", function (event) {
     const number1Value = number1Input.value;
     const number2Value = number2Input.value;
 
-    if (number1Value === "" || number2Value === "") {
-        alert("введите значение")
-        return number1Value || number2Value
-    }
-    sumInput.value = +number1Input.value + +number2Input.value;
 
+    const error = document.querySelector("#error");
+    if (number1Value === "") {
+        error.innerText = "Поле пустое-запоните"
+        return number1Value || number2Value;
+    };
+    if (number2Value === "") {
+        error.innerText = "Поле пустое-запоните"
+        return number1Value || number2Value;
+        
+    };
+    resultInput.value = +number1Input.value + +number2Input.value;
 
+    number1Input.value = "";
+    number2Input.value = "";
 });
 
+
+// const radioInput = document.querySelector("input[name='sign']:checked");
+// const radioInputValue = radioInput.value;
 
 
