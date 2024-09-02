0933faaa58daaa051b883ca19545d74a9f4a5cf5
annagrnva <m-ippolitova@mail.ru>
2024-09-02 22:35:17 +0300

    task with boxes changing colors

--- DOM js/boxes changing colors/index.mjs
+++ DOM js/boxes changing colors/index.mjs
@@ -1,21 +1,41 @@
-const box = document.querySelectorAll(".box");
-
-setTimeout(() => {
-  box.style.backgroundColor = 'red';
-}, 2000);
-
-console.log(box)
-
-
-
-// const myFunc = () => {
-
-//   boxes.forEach((box) => {
-//     box.classList.add("red");
-//   });
-
-// };
-
-// boxes.forEach(function (item) {
-//   item.addEventListener("click", myFunc);
+const box = document.querySelector(".box");
+//вывод рандомных цветов по клику
+// box.addEventListener("click", () => {
+//   setInterval(() => {
+//   box.style.backgroundColor = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padEnd(6, '0');
+// }, 2000);
 // });
+
+
+//вывод двух цветов с условием
+// const blue = "blue";
+// const green = "green";
+
+// function animateBox() {
+//   const changeBox = box.style.backgroundColor;
+
+//   if (changeBox === green) {
+//     box.style.backgroundColor = blue;
+//   } else {
+//     box.style.backgroundColor = green
+//   }
+// }
+
+// setInterval(animateBox, 1000)
+
+
+// вывод массива элементов по порядку (по счетчику)
+let colors = ['red', 'green', 'blue', 'yellow'];
+let count = 0;
+
+box.addEventListener("click", animateBox) 
+  function animateBox() {
+  if (count === colors.length ) {
+    count = 0;
+  } else {
+    count ++;
+  }
+  box.style.background = colors[count];
+
+}
+setInterval(animateBox, 1000)
