3cbf6a186bbd18ed298e4d4b73138a5add7090e5
annagrnva <m-ippolitova@mail.ru>
2024-09-18 00:05:52 +0300

    adding delete btn

--- DOM js/ToDoList/index.mjs
+++ DOM js/ToDoList/index.mjs
@@ -7,32 +7,53 @@ const counterTasksNum = document.querySelector("#counter-tasks-num");
 const counterDone = document.querySelector("#counter-done");
 const counterDoneNum = document.querySelector("#counter-done-num");
 
+let count = 1;
 
-
-// console.log(container)
+// добавление пунктов по клику
 container.addEventListener("submit", (event) => {
   event.preventDefault()
   const inputValue = inputText.value;
-  if(inputValue.length === 0) {
+  if (inputValue.length === 0) {
     return inputValue
   };
 
   const newElement = document.createElement('div');
   newElement.innerHTML = `
 <div>
+<input type="radio" class="radio-button" name="radio"/>
 <span>${inputValue}</span>
 <button class="newElement-edit-button">Edit</button>
-<button class="newElement-delete-button">Delete</button>
+<button class="newElement-delete-button-${count}">Delete</button>
 
 </div>
 `
-result.appendChild(newElement);
-  // console.log(newElement)
+  result.appendChild(newElement);
 
-  counterTasksNum.textContent = ++ counterTasksNum.textContent;
-  // counterDoneNum.textContent = ++ counterDoneNum.textContent;
+  // удаление пунктов
+  const deleteButton = document.querySelector(`.newElement-delete-button-${count}`)
+  deleteButton.addEventListener('click', () => {
+    newElement.remove();
+    count--
 
-
+  })
+  count++
   inputText.value = "";
+
+  // счетчик инпута Сколько всего задач
+
+  counterTasksNum.textContent = ++counterTasksNum.textContent;
+  
+  // счетчик радио-кнопки при нажатии Выполнено
+  const radioInput = document.querySelector("input[name='radio']")
+  radioInput.addEventListener('click', () => {
+    // if(counterDoneNum) {
+
+    // }
+   // counterDoneNum.textContent = ++counterDoneNum.textContent;
+  
+
+
+  })
+
 })
 
