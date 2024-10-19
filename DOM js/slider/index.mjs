b9e9232379c3f6dc8ec04b60f162516145fd3835
annagrnva <m-ippolitova@mail.ru>
2024-10-20 00:14:44 +0300

    slider

---
+++ DOM js/slider/index.mjs
@@ -0,0 +1,10 @@
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+
+let slideNumber = 1;
+
+right.addEventListener("click", () => {
+  slider.style.transform = `translateX(-${slideNumber * 600}px)`
+  slideNumber++
+})
