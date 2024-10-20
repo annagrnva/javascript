c6f8a003979767240981c5a2788136ab770de13d
annagrnva <m-ippolitova@mail.ru>
2024-10-21 00:52:34 +0300

    slider

--- DOM js/slider/index.mjs
+++ DOM js/slider/index.mjs
@@ -1,10 +1,23 @@
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
+const imgs = document.querySelectorAll(".img-size")
 
 let slideNumber = 1;
 
+function showSlide() {
+
+ imgs.forEach(img => {
+  img.classList.add("img-block")
+ });
+    
+  
+  
+}
+
 right.addEventListener("click", () => {
-  slider.style.transform = `translateX(-${slideNumber * 600}px)`
-  slideNumber++
+  slideNumber + 1 
+
+  showSlide(slideNumber)
 })
+
