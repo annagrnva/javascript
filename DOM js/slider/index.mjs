5d2bdf622da7ca237e2e9cc67b0920a59149384e
annagrnva <m-ippolitova@mail.ru>
2024-10-27 00:04:04 +0300

    add buttom btns to slider

--- DOM js/slider/index.mjs
+++ DOM js/slider/index.mjs
@@ -2,12 +2,17 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const imgWrapper = document.querySelectorAll(".img-wrapper");
 const imgs = document.querySelectorAll(".img")
+const buttons = document.querySelectorAll(".button")
 
 let slideNumber = 0;
+
 imgs[slideNumber].classList.add('activeImg');
+buttons[slideNumber].classList.add('activeBtns');
 
 right.addEventListener("click", () => {
 
+buttons[slideNumber].classList.remove('activeBtns');
+  
   slideNumber++
   if (slideNumber >= imgs.length) {
     slideNumber = 0
@@ -15,11 +20,15 @@ right.addEventListener("click", () => {
   imgs.forEach(img => {
     img.classList.remove("activeImg")
   });
+    imgs[slideNumber].classList.add("activeImg")
+  buttons[slideNumber].classList.add('activeBtns');
 
-  imgs[slideNumber].classList.add("activeImg")
+
 });
 
 left.addEventListener("click", () => {
+
+buttons[slideNumber].classList.remove('activeBtns');
 
   slideNumber--
   if (slideNumber < 0 ) {
@@ -30,5 +39,9 @@ left.addEventListener("click", () => {
   });
   
   imgs[slideNumber].classList.add("activeImg")
+  buttons[slideNumber].classList.add('activeBtns');
 
 });
+ 
+
+
