400937ef59fe7315688cac59bab682bae98cb651
annagrnva <m-ippolitova@mail.ru>
2024-11-02 23:48:06 +0300

    quiz questions and answers

--- DOM js/quiz/index.mjs
+++ DOM js/quiz/index.mjs
@@ -1,20 +1,61 @@
-const question = [
+const questions = [
   {
     question: "When do children go to school?",
     answers: [
-      {text: "7yo", correct: true},
-      {text: "4yo", correct: true},
-      {text: "10yo", correct: true},
-    ]
+      { text: "7yo" }, 
+      { text: "4yo" }, 
+      { text: "10yo" }
+    ],
+    // correct: 1,
   },
 ];
 
-const questionElement = document.querySelector(".question")
-const answerElement = document.querySelectorAll(".answer-btns")
-let questionCounter = 0;
-let answerCounter = 0;
+const quiz = document.querySelector(".quiz-container")
 
-function showQuestion() {
-  questionElement.textContent = question[questionCounter].question
-}
-showQuestion()
+questions.forEach(question => {
+  const questionElement = document.createElement('div');
+  questionElement.innerHTML =
+    `<div>${question.question}</div>`;
+
+  question.answers.forEach(answer => {
+    const answerElement = document.createElement('div');
+    answerElement.innerHTML = 
+    `<input type="radio" name="${question.answers}"> ${answer.text}`
+
+    questionElement.appendChild(answerElement)
+//  console.log(answer)
+
+  });
+
+  quiz.appendChild(questionElement)
+});
+
+const button = document.createElement('button')
+button.classList.add('button')
+button.textContent = 'next'
+button.addEventListener('click', () => {
+  console.log('sadf')
+})
+document.body.append("button")
+
+
+// const questionElement = document.querySelector(".question")
+// const answerElement = document.querySelectorAll(".answer-btns")
+// let questionCounter = 0;
+// let answerCounter = 0;
+
+// function showQuestion() {
+//   questionElement.textContent = question[questionCounter].question
+// }
+// showQuestion()
+
+// for(answer of question[questionCounter]['answers']) {
+// console.log(answer)
+// }
+
+
+// questionElement.forEach((answer) => {
+//   question[questionCounter]['answers']
+
+//   console.log(answer)
+// });
