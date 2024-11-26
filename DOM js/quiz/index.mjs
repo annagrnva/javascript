ca334d4563dda3e652c2f866c3bd971737132f9c
annagrnva <m-ippolitova@mail.ru>
2024-11-27 00:32:31 +0300

    quiz next button

--- DOM js/quiz/index.mjs
+++ DOM js/quiz/index.mjs
@@ -2,7 +2,7 @@ const questions = [
   {
     question: "When do children go to school?",
     answers: ["7yo", "4yo", "10yo"],
-    correct: '7yo',
+    correct: 1,
   },
 
   {
@@ -12,15 +12,17 @@ const questions = [
   },
 ];
 
+
+const quiz = document.querySelector(".quiz-container");
+const nextBtn = document.querySelector('.next-btn');
+const questionElement = document.createElement('div');
+const answerElement = document.createElement('div');
+const radioChecked = document.querySelector('input[type="radio"]:checked');
+
 let questionIndex = 0;
 let answerNumber = 0;
 let correctAnswers = 0;
 let falseAnswers = 0;
-const quiz = document.querySelector(".quiz-container")
-const questionElement = document.createElement('div');
-const answerElement = document.createElement('div');
-const radioChecked = document.querySelector('input[type="radio"]:checked')
-
 
 function showQuestion() {
   const questionData = questions[questionIndex];
@@ -28,10 +30,10 @@ function showQuestion() {
   questionElement.innerHTML =
     `<div>${questionData.question}</div>`;
 
-  questionData.answers.forEach(answer => {
+  questionData.answers.forEach((answer, index) => {
     const answerElement = document.createElement('div');
     answerElement.innerHTML =
-      `<input class="radio-btn" type="radio" value="${questionData.answers[answerNumber]}
+      `<input class="radio-btn" type="radio" value="${questionData.answers[index++]}
       "name="${questionData.answers}"> ${answer}`
 
     questionElement.appendChild(answerElement);
@@ -40,26 +42,8 @@ function showQuestion() {
 
 };
 
-function clearQuestion() {
-  questionElement.innerHTML = '';
-  answerElement.innerHTML = '';
-  // if (questionIndex <= questions.length - 1) {
-  //     questionIndex++;
-  //     questionElement.textContent = questions[questionIndex]
-  //   }
-
-};
-
 const inputError = document.createElement('div');
 document.body.append(inputError);
-
-const button = document.createElement('div');
-button.innerHTML = `
-<div class="container-btn">
-<button class="button">next</button>
-</div>
-`
-document.body.append(button);
 
 function showError() {
   const radioChecked = document.querySelector('input[type="radio"]:checked');
@@ -87,15 +71,101 @@ function showResults() {
   document.body.appendChild(result);
 };
 
-
-showQuestion()
-button.addEventListener('click', () => {
+nextBtn.addEventListener('click', () => {
   showError()
-  // questionElement.textContent = questions[questionIndex + 1];
-  clearQuestion()
-  if (questionIndex <= questions.length - 1) {
-    questions[questionIndex + 1];
-    showQuestion()
+  if (questionIndex === questions.length - 1) {
+    questionIndex = 0;
+  } else {
+    questionIndex += 1;
+    showQuestion(questionIndex)
   };
 });
 showResults()
+
+const init = () => {
+  showQuestion(0)
+}
+init()
+
+
+
+{/* <div id="app">
+  <div id="book">
+
+  </div>
+  <div>
+    <div>
+      <button id="prevButton">Назад</button>
+      <button id="nextButton">Вперед</button>
+    </div>
+    <button id="selectButton">Выбрать</button>
+
+
+    <div>
+      Ваш выбор: <b id="userChoice"></b>
+    </div>
+  </div>
+
+  <script>
+    const book = document.querySelector('#book');
+    const prevButton = document.querySelector('#prevButton');
+    const nextButton = document.querySelector('#nextButton');
+
+    const choiseField = document.querySelector('#userChoice');
+    const selectButton = document.querySelector('#selectButton');
+
+    let currentBook = 0;
+
+    const books = [
+      {
+        title: 'Война и Мир',
+        description: "Много страниц, людей, активностей ...",
+        pages: '999',
+				id: 101,
+      },
+      {
+        title: 'Преступление и наказание',
+        description: "Страх и ненависть в Питере",
+        pages: '500',
+				id: 777,
+      },
+      {
+        title: 'Нравственные письма Сеннеки Луцилию ',
+        description: "Философия..",
+        pages: '300',
+				id: 50,
+      }
+    ]
+
+
+    const insertBook = (bookNumber) => {
+      book.innerHTML = `
+<div>Заголовок: ${books[bookNumber].title}</div>
+  <div>Описание: ${books[bookNumber].description}</div>
+  <div>Количество страниц: ${books[bookNumber].pages}</div>
+</div>
+`
+    }
+
+    nextButton.addEventListener('click', () => {
+      if (currentBook === books.length - 1) {
+        currentBook = 0;
+      } else {
+        currentBook += 1;
+      }
+
+      insertBook(currentBook);
+    })
+
+
+    selectButton.addEventListener('click', () => {
+				choiseField.innerHTML = books[currentBook].title;
+    })
+
+
+    const init = () => {
+      insertBook(0);
+    }
+
+
+    init(); */}
