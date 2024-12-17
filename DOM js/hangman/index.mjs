609a27d78836a812c8461616150b31cfa63960ae
annagrnva <m-ippolitova@mail.ru>
2024-12-17 23:40:03 +0300

    hangman html & css

---
+++ DOM js/hangman/index.mjs
@@ -0,0 +1,20 @@
+const words = [
+  {
+    question: 'the capital of GB',
+    word: 'london'
+  },
+  {
+    question: 'the capital of Russia',
+    word: 'moscow'
+  },
+];
+
+const albhabet = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+  'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+  'y', 'z'
+];
+
+let liveCounter = 0;
+let usersAnswers = [];
