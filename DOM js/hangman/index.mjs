152467af642c60ac04a9de3e5a2a0e2153fbefb7
annagrnva <m-ippolitova@mail.ru>
2024-12-26 00:08:24 +0300

    showAlphabet func using js array

--- DOM js/hangman/index.mjs
+++ DOM js/hangman/index.mjs
@@ -1,16 +1,16 @@
 const word = 'london';
 
-// const alphabet = ['a', 'b', 'c', 'd', 
-// 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-// 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-// 'u', 'v', 'w', 'x', 'y', 'z'];
+const alphabet = ['a', 'b', 'c', 'd',
+  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+  'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y', 'z'];
 
 
 let liveCounter = 0;
 let usersAnswers = [];
 const wordField = document.querySelector(".hangman-word");
-//const alphabetBtn = document.querySelector(".hangman-alphabet_btns")
-const btns = document.querySelectorAll(".btn")
+const alphabetBtn = document.querySelector(".hangman-alphabet_btns")
+// const btns = document.querySelectorAll(".btn")
 
 function showField() {
   let field = word.split('')
@@ -28,28 +28,31 @@ function showField() {
   console.log(field)
 };
 
-function showAlphabet() {
-  btns.forEach(btn => {
-  btn.addEventListener('click', (btn) => {
-    let currentBtn = btn.currentTarget
-    console.log(currentBtn)
-  })
-})
-}
-
-
-
 // function showAlphabet() {
-//   alphabetBtn.innerHTML = alphabet
-//   console.log(alphabetBtn)
-
-//   alphabetBtn.addEventListener("click", () => {
-
+//   btns.forEach(btn => {
+//   btn.addEventListener('click', (btn) => {
+//     let currentBtn = btn.currentTarget
+//     console.log(currentBtn)
+//     //  wordField.textContent += currentBtn
 //   })
+// })
 // }
 
+function showAlphabet() {
+  alphabet.forEach(letter => {
+    const button = document.createElement('div');
+    button.classList.add('btn')
+    button.textContent = letter;
+
+    button.addEventListener('click', () => {
+      wordField.textContent += letter
+    })
+    alphabetBtn.appendChild(button)
+  })
+}
+
 const init = () => {
+  showAlphabet()
 };
 init();
 showField()
-showAlphabet()
